af7a6d0d2a9fd0c8226eb6afba475d4d89ead7dc
kiln707 <steven.guy.swanson@gmail.com>
2019-04-02 14:58:25 -0700

    Added FilestoreSessionDatastore

---
+++ flask_protect/Session/filestore_session_datastore.py
@@ -0,0 +1,24 @@
+class FilestoreSessionDatastore(SessionDatastoreMixin):
+    def __init__(self, path, secret):
+        self.path=path
+        self.secret=secret
+
+    def create(self):
+        'Create a new session'
+        raise NotImplementedError
+
+    def exists(self, sid):
+        'Does the given session-id exist?'
+        raise NotImplementedError
+
+    def remove(self, sid):
+        'Remove the session'
+        raise NotImplementedError
+
+    def get(self, sid, digest):
+        'Retrieve a managed session by session-id, checking the HMAC digest'
+        raise NotImplementedError
+
+    def put(self, session):
+        'Store a managed session'
+        raise NotImplementedError
