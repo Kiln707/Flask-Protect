c7ab8491497101187225b0383a7635ab8065ab52
kiln707 <steven.guy.swanson@gmail.com>
2019-04-24 21:12:18 -0700

    Adding default messages

--- flask_protect/Authentication/userpass/validator.py
+++ flask_protect/Authentication/userpass/validator.py
@@ -25,6 +25,83 @@ class UserPassValidator(ValidatorMixin):
             'DEPRECIATED_SCHEMES':["auto"], #By Default, Depreciate all schemes except default
             'TRUNCATE_ERROR':False, #Silently truncate password.
         }
+        'MSGS': {
+            'UNAUTHORIZED': (
+                _('You do not have permission to view this resource.'), 'error'),
+            'CONFIRM_REGISTRATION': (
+                _('Thank you. Confirmation instructions '
+                  'have been sent to %(email)s.'),
+                'success'),
+            'EMAIL_CONFIRMED': (
+                _('Thank you. Your email has been confirmed.'), 'success'),
+            'ALREADY_CONFIRMED': (
+                _('Your email has already been confirmed.'), 'info'),
+            'INVALID_CONFIRMATION_TOKEN': (
+                _('Invalid confirmation token.'), 'error'),
+            'EMAIL_ALREADY_ASSOCIATED': (
+                _('%(email)s is already associated with an account.'), 'error'),
+            'PASSWORD_MISMATCH': (
+                _('Password does not match'), 'error'),
+            'RETYPE_PASSWORD_MISMATCH': (
+                _('Passwords do not match'), 'error'),
+            'INVALID_REDIRECT': (
+                _('Redirections outside the domain are forbidden'), 'error'),
+            'PASSWORD_RESET_REQUEST': (
+                _('Instructions to reset your password have been sent to %(email)s.'),
+                'info'),
+            'PASSWORD_RESET_EXPIRED': (
+                _('You did not reset your password within %(within)s. '
+                  'New instructions have been sent to %(email)s.'), 'error'),
+            'INVALID_RESET_PASSWORD_TOKEN': (
+                _('Invalid reset password token.'), 'error'),
+            'CONFIRMATION_REQUIRED': (
+                _('Email requires confirmation.'), 'error'),
+            'CONFIRMATION_REQUEST': (
+                _('Confirmation instructions have been sent to %(email)s.'), 'info'),
+            'CONFIRMATION_EXPIRED': (
+                _('You did not confirm your email within %(within)s. '
+                  'New instructions to confirm your email have been sent '
+                  'to %(email)s.'), 'error'),
+            'LOGIN_EXPIRED': (
+                _('You did not login within %(within)s. New instructions to login '
+                  'have been sent to %(email)s.'), 'error'),
+            'LOGIN_EMAIL_SENT': (
+                _('Instructions to login have been sent to %(email)s.'), 'success'),
+            'INVALID_LOGIN_TOKEN': (
+                _('Invalid login token.'), 'error'),
+            'DISABLED_ACCOUNT': (
+                _('Account is disabled.'), 'error'),
+            'EMAIL_NOT_PROVIDED': (
+                _('Email not provided'), 'error'),
+            'INVALID_EMAIL_ADDRESS': (
+                _('Invalid email address'), 'error'),
+            'PASSWORD_NOT_PROVIDED': (
+                _('Password not provided'), 'error'),
+            'PASSWORD_NOT_SET': (
+                _('No password is set for this user'), 'error'),
+            'PASSWORD_INVALID_LENGTH': (
+                _('Password must be at least 6 characters'), 'error'),
+            'USER_DOES_NOT_EXIST': (
+                _('Specified user does not exist'), 'error'),
+            'INVALID_PASSWORD': (
+                _('Invalid password'), 'error'),
+            'PASSWORDLESS_LOGIN_SUCCESSFUL': (
+                _('You have successfully logged in.'), 'success'),
+            'FORGOT_PASSWORD': (
+                _('Forgot password?'), 'info'),
+            'PASSWORD_RESET': (
+                _('You successfully reset your password and you have been logged in '
+                  'automatically.'), 'success'),
+            'PASSWORD_IS_THE_SAME': (
+                _('Your new password must be different than your previous password.'),
+                'error'),
+            'PASSWORD_CHANGE': (
+                _('You successfully changed your password.'), 'success'),
+            'LOGIN': (
+                _('Please log in to access this page.'), 'info'),
+            'REFRESH': (
+                _('Please reauthenticate to access this page.'), 'info'),
+        }
     }
 
     def __init__(self, datastore, crypt_context=None, **kwargs):
