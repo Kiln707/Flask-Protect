d8be9d1ba18c24203ef75eec51897dbd88853c19
kiln707 <steven.guy.swanson@gmail.com>
2019-04-25 14:32:05 -0700

    Work on implementing forms

--- flask_protect/Authentication/form_validators.py
+++ flask_protect/Authentication/form_validators.py
@@ -1,8 +1,33 @@
 from builtins import object
-from ..utils import _protect
+from .utils import _datastore
 
 class FormValidatorMixin(object):
     def __call__(self, form, field):
         if self.message and self.message.isupper():
             self.message = _protect.get_message(self.message)[0]
         return super(FormValidatorMixin, self).__call__(form, field)
+
+class EqualTo(ValidatorMixin, validators.EqualTo):
+    pass
+class Required(ValidatorMixin, validators.DataRequired):
+    pass
+class Email(ValidatorMixin, validators.Email):
+    pass
+class Length(ValidatorMixin, validators.Length):
+    pass
+
+
+email_required = Required(message='EMAIL_NOT_PROVIDED')
+email_validator = Email(message='INVALID_EMAIL_ADDRESS')
+password_required = Required(message='PASSWORD_NOT_PROVIDED')
+password_length = Length(min=6, max=128, message='PASSWORD_INVALID_LENGTH')
+
+def unique_user_email(form, field):
+    if _datastore.get_user(field.data) is not None:
+        msg = get_message('EMAIL_ALREADY_ASSOCIATED', email=field.data)[0]
+        raise ValidationError(msg)
+
+def valid_user_email(form, field):
+    form.user = _datastore.get_user(field.data)
+    if form.user is None:
+raise ValidationError(get_message('USER_DOES_NOT_EXIST')[0])
--- flask_protect/Authentication/forms.py
+++ flask_protect/Authentication/forms.py
@@ -1,2 +1,7 @@
-class RegisterFormMixin():
-    pass
+class BaseForm()
+    def __init__(self, *args, **kwargs):
+        if current_app.testing:
+            self.TIME_LIMIT = None
+        super(Form, self).__init__(*args, **kwargs)
+
+class 
