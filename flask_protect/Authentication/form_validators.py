6b612bf0638dd781531607d7ba454e20efa7f588
kiln707 <steven.guy.swanson@gmail.com>
2019-04-24 21:12:35 -0700

    Work on validators

--- flask_protect/Authentication/form_validators.py
+++ flask_protect/Authentication/form_validators.py
@@ -4,4 +4,5 @@ from ..utils import _protect
 class FormValidatorMixin(object):
     def __call__(self, form, field):
         if self.message and self.message.isupper():
-            self.message = get_message(self.message)[0]
+            self.message = _protect.get_message(self.message)[0]
+        return super(FormValidatorMixin, self).__call__(form, field)
