cbd2cbb489fc472583f607843e20d0e746ffa4af
Kiln707 <steven.guy.swanson@gmail.com>
2019-09-18 16:38:58 -0700

    Further writing of is_safe_url for very thorough checks

--- flask_protect/utils.py
+++ flask_protect/utils.py
@@ -1,7 +1,7 @@
 from flask import current_app, request, session, after_this_request, url_for
 from werkzeug.local import LocalProxy
 
-import datetime
+import datetime, socket, unicodedata
 from itsdangerous import URLSafeTimedSerializer
 
 from ._compat import urlsplit, urlparse
@@ -16,115 +16,98 @@ def get_url(endpoint_or_url):
     except:
         return endpoint_or_url
 
+class UnsafeURLError(Exception):
+    code_msg = {
+    0: 'No URL present',
+    1: 'Invalid characters in URL',
+    2: 'Unsafe browser behavior with starting ///',
+    3: 'Invalid IP Address',
+    4: 'No hostname with scheme and path',
+    5: 'Starts with unicode control character, causes undetermined behavior',
+    6: 'HTTPS required! URL has unallowed scheme',
+    7: 'URL has unallowed scheme',
+    8: 'URL contains username and/or password which was not allowed',
+    9: 'URL contains unallowed hostname'
+    }
+    def __init__(self, code):
+        self.code = code
+        self.message = self.code_msg[code]
 
-# def is_safe_url(url, allowed_hosts, require_https=False):
-#     """
-#     Return ``True`` if the url is a safe redirection (i.e. it doesn't point to
-#     a different host and uses a safe scheme).
-#
-#     Always return ``False`` on an empty url.
-#
-#     If ``require_https`` is ``True``, only 'https' will be considered a valid
-#     scheme, as opposed to 'http' and 'https' with the default, ``False``.
-#     """
-#     if url is not None:
-#         url = url.strip()
-#     if not url:
-#         return False
-#     if allowed_hosts is None:
-#         allowed_hosts = set()
-#     # Chrome treats \ completely as / in paths but it could be part of some
-#     # basic auth credentials so we need to check both URLs.
-#     return (_is_safe_url(url, allowed_hosts, require_https=require_https) and
-#             _is_safe_url(url.replace('\\', '/'), allowed_hosts, require_https=require_https))
+def valid_ipv6(address):
+    # From Joe Hildebrand and the Tin man
+    # https://stackoverflow.com/questions/53497/regular-expression-that-matches-valid-ipv6-addresses
+    try:
+        socket.inet_pton(socket.AF_INET6, address)
+        return True
+    except socket.error:
+        return False
 
-# def _is_safe_url(url, allowed_hosts, require_https=False):
-#     # Chrome considers any URL with more than two slashes to be absolute, but
-#     # urlparse is not so flexible. Treat any url with three slashes as unsafe.
-#     if url.startswith('///'):
-#         return False
-#     try:
-#         url_info = _urlparse(url)
-#     except ValueError:  # e.g. invalid IPv6 addresses
-#         return False
-#     # Forbid URLs like http:///example.com - with a scheme, but without a hostname.
-#     # In that URL, example.com is not the hostname but, a path component. However,
-#     # Chrome will still consider example.com to be the hostname, so we must not
-#     # allow this syntax.
-#     if not url_info.netloc and url_info.scheme:
-#         return False
-#     # Forbid URLs that start with control characters. Some browsers (like
-#     # Chrome) ignore quite a few control characters at the start of a
-#     # URL and might consider the URL as scheme relative.
-#     if unicodedata.category(url[0])[0] == 'C':
-#         return False
-#     scheme = url_info.scheme
-#     # Consider URLs without a scheme (e.g. //example.com/p) to be http.
-#     if not url_info.scheme and url_info.netloc:
-#         scheme = 'http'
-#     valid_schemes = ['https'] if require_https else ['http', 'https']
-#     return ((not url_info.netloc or url_info.netloc in allowed_hosts) and
-#             (not scheme or scheme in valid_schemes))
-
-
-def is_safe_url(url, allowed_hosts=set(), external='', require_https=False, allow_userpass=False, allowed_schemes=['http', 'https']):
-    def safe(url, allowed_hosts, external, require_https, allow_userpass, allowed_schemes):
-        import unicodedata
+def is_safe_url(url, allowed_hosts=[], require_https=False, allow_userpass=False, allowed_schemes=['http', 'https'], pass_Exception=False):
+    def safe(url):
         # Chrome considers any URL with more than two slashes to be absolute, but
         # urlparse is not so flexible. Treat any url with three slashes as unsafe.
         if url.startswith('///'):
-            return False
+            return (False, UnsafeURLError(2)) if pass_Exception else False
         try:
             url_info = urlparse(url)
         except ValueError:  # e.g. invalid IPv6 addresses
-            return False
+            return (False, UnsafeURLError(3)) if pass_Exception else False
+        print(url_info)
         # Forbid URLs like http:///example.com - with a scheme, but without a hostname.
         if not url_info.netloc and url_info.scheme:
-            return False
-
+            return (False, UnsafeURLError(4)) if pass_Exception else False
         # Forbid URLs that start with control characters. Some browsers (like
         # Chrome) ignore quite a few control characters at the start of a
         # URL and might consider the URL as scheme relative.
         if unicodedata.category(url[0])[0] == 'C':
-            return False
-
+            return (False, UnsafeURLError(5)) if pass_Exception else False
         scheme = url_info.scheme
-        # Consider URLs without a scheme (e.g. //example.com/p) to be http.
-        if not url_info.scheme and url_info.netloc:
-            scheme = 'http'
-        if require_https and url_info.scheme != 'https':
-            return False
+        # Consider URLs without a scheme (e.g. //example.com/p) to be http or https.
+        if not scheme and url_info.netloc:
+            scheme = 'https' if require_https else 'http'
+        # Fail if https is required and scheme is not https or not allowed scheme
+        if require_https and scheme != 'https':
+            return (False, UnsafeURLError(6)) if pass_Exception else False
+        elif scheme and scheme not in allowed_schemes:
+            return (False, UnsafeURLError(7)) if pass_Exception else False
+        # If we don't want to allow username/passwords in the url, ensure there is no username/password
+        if not allow_userpass and (url_info.username or url_info.password):
+            return (False, UnsafeURLError(8)) if pass_Exception else False
+        host_url = urlparse(request.host_url)
+        hostname = url_info.hostname
 
+        # If IPv6 is used, ensure it is grabbed
+        if '@' in url_info.netloc:
+            netloc_split = url_info.netloc.split('@')[1]
+        else:
+            netloc_split = url_info.netloc
+        netloc_split = netloc_split.split('.')
+        for partial in netloc_split:
+            if valid_ipv6(partial):
+                hostname = '.'.join(netloc_split)
 
-
-
-        return ((not url_info.netloc or url_info.netloc in allowed_hosts) and
-                (not scheme or scheme in valid_schemes))
-
-        print("url: ", url)
-        print("scheme: ",url_info.scheme) #will need to check
-        print("netloc: ",url_info.netloc)
-        print("path: ",url_info.path) #If @ in path, no prepended scheme
-        print("params: ",url_info.params) #Sub-pages
-        print("query: ",url_info.query) #Will leave alone
-        print("fragment: ",url_info.fragment) #Safe, internal page reference (AKA Anchors)
-        print("username: ",url_info.username) # Will discourage, but make allowable to use username, password
-        print("password: ",url_info.password)
-        print("hostname: ",url_info.hostname) # Will need to validate
-        print("port: ",url_info.port) #Safe, will ignore
-        print("---------------------------------------")
-        return True
+        # If the path contains the actual hostname (Or what would be treated as hostname), set hostname to path
+        if not hostname and ( '.' in url_info.path and not url_info.path.startswith( ('.', '/', ';', '#', '?') ) ):
+            hostname = url_info.path
+        # return whether this url hostname is this server OR is one of the allowed hostnames
+        if hostname and not (hostname == host_url.hostname or hostname in allowed_hosts):
+            print(hostname, hostname in allowed_hosts)
+            return (False, UnsafeURLError(9)) if pass_Exception else False
+        # Relative Path is safe
+        return (True, None) if pass_Exception else True
     if url is not None:
         url = url.strip()
     if not url:
-        return False
+        return (False, UnsafeURLError(0)) if pass_Exception else False
     # If url is not an actual url, but an endpoint
     # It is safe to trust.
     try:
         current_app.url_map.iter_rules(url)
-        return True
+        return (True, None) if pass_Exception else True
     except KeyError:
-        return ((safe(url, allowed_hosts, external, require_https, allow_userpass, allowed_schemes)) and (safe(url.replace('\\','/'), allowed_hosts, external, require_https, allow_userpass, allowed_schemes)))
+        if any( chr in url for chr in ('<', '>', '`', '^')):
+            return (False, UnsafeURLError(1)) if pass_Exception else False
+        return (safe(url) and safe(url.replace('\\','/')))
 
 #
 #   Next Parameter methods
