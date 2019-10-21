af9476a3b2ff2b72ec54524cfa7d9161f8aef219
Kiln707 <steven.guy.swanson@gmail.com>
2019-10-21 16:16:21 -0700

    Changing mail mixin to allow user customization. Update to unittest

--- flask_protect/validator/mixins/flaskmail_validator_mixin.py
+++ flask_protect/validator/mixins/flaskmail_validator_mixin.py
@@ -1,16 +1,12 @@
 from .validator_base import ValidatorMixin
 
 class FMail_Mixin(ValidatorMixin):
-    def send_mail(self, action, user, **context):
+    def send_mail(self, recipients, sender, subject, template, plaintext=True, html=True, **context):
         mail=current_app.extensions.get('mail')
         if mail:
-            subject=_validator.get_config('EMAIL_SUBJECT')[action]
-            recipient=getattr(user, self.get_user_field('EMAIL'))
-            msg = Message(subject=subject, sender=self.get_config('EMAIL_SENDER'), recipients=[recipient])
-            if self.get_config('EMAIL_PLAINTEXT'):
-                template=self.get_config('EMAIL_TXT_TEMPLATE')[action]
+            msg=Message(subject=subject, sender=from, recipients=recipients)
+            if plaintext:
                 msg.body=render_template(template, **context)
-            if self.get_config('EMAIL_HTML'):
-                template=self.get_config('EMAIL_HTML_TEMPLATE')[action]
+            if html:
                 msg.html=render_template(template, **context)
             mail.send(msg)
--- tests/test_ValidatorMixins.py
+++ tests/test_ValidatorMixins.py
@@ -6,9 +6,28 @@ def test_imports():
         CryptContextValidatorMixin, ValidatorMixin
 
 class FlaskMail_Validator_Mixin_TestCase(unittest.TestCase):
-    def setUp(self):
-        self.app = Flask(__name__)
+    from flask_protect import Protect, validator
+    class FlaskMail_TestValidator(validator.Fmail_Mixin):
+        def send_mail_route(self):
+            self.send_mail()
 
+        def routes(self, blueprint):
+            blueprint.add_url_rule(rule='/send_mail', endpoint='send_mail', view_func=self.send_mail_route)
+
+    def setUp(self):
+        try:
+            from flask_mail import Mail
+        except ImportError:
+            self.assertTrue(False)
+        self.app = Flask(__name__)
+        self.app.testing = True
+        self.mail = Mail(self.app)
+        self.ctx = self.app.test_request_context()
+        self.ctx.push()
+        self.outbox = self.mail.record_messages()
+
+    def testDown(self):
+        self.ctx.pop()
 
 
 def test_FlaskMail_Validate_mixin():
